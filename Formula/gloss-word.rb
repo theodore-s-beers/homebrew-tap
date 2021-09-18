0380b21f3f195f9b120d902766dc50f986cb1d27
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2021-09-19 00:16:44 +0200

    Try to bump to 0.1.5

--- Formula/gloss-word.rb
+++ Formula/gloss-word.rb
@@ -1,14 +1,9 @@
 class GlossWord < Formula
   desc "English dictionary lookup utility"
   homepage "https://github.com/theodore-s-beers/gloss-word"
-  url "https://github.com/theodore-s-beers/gloss-word/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "89406aca18e7e22346097ebe7d916e06c0bb03cc261d1e00209e25f9f7d70668"
+  url "https://github.com/theodore-s-beers/gloss-word/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "0530365b54e1db118245a6826b01b0896385b4646fbb079b7bf6ebb14b32f91b"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/theodore-s-beers/homebrew-tap/releases/download/gloss-word-0.1.1"
-    sha256 cellar: :any_skip_relocation, catalina: "bb24e4e2f00686996c474814439396025a204ea6170b92dee3d823f53942cd2d"
-  end
 
   depends_on "rust" => :build
 
--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Or `brew tap theodore-s-beers/tap` and then `brew install <formula>`.
 
 ## Documentation
 
-`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh/).
