f92f8b951f4fce73462d622da7f47eb4382e8675
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2021-11-02 14:02:22 +0000

    gloss-word: add 0.1.6 bottle.

--- Formula/gloss-word.rb
+++ Formula/gloss-word.rb
@@ -5,6 +5,11 @@ class GlossWord < Formula
   sha256 "f7431229b8f524f48f56f4a9327f2f4896356231fa0f7ff1886cd85a6f2a9b85"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/theodore-s-beers/homebrew-tap/releases/download/gloss-word-0.1.6"
+    sha256 cellar: :any_skip_relocation, catalina: "8f3e11169fd13109000f421807ae8eabd745b4fe4fc9a6193f2ed8148ca9d37a"
+  end
+
   depends_on "rust" => :build
 
   def install
