9557224114f7e62f5f5f761e1696c1eed8d46ebd
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2021-11-02 14:16:12 +0100

    Bump gloss-word to 0.1.6

--- Formula/gloss-word.rb
+++ Formula/gloss-word.rb
@@ -1,14 +1,9 @@
 class GlossWord < Formula
   desc "English dictionary lookup utility"
   homepage "https://github.com/theodore-s-beers/gloss-word"
-  url "https://github.com/theodore-s-beers/gloss-word/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "0530365b54e1db118245a6826b01b0896385b4646fbb079b7bf6ebb14b32f91b"
+  url "https://github.com/theodore-s-beers/gloss-word/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "f7431229b8f524f48f56f4a9327f2f4896356231fa0f7ff1886cd85a6f2a9b85"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/theodore-s-beers/homebrew-tap/releases/download/gloss-word-0.1.5"
-    sha256 cellar: :any_skip_relocation, catalina: "ceed7c83fdc5625d336915c45fb8f34a3f362428ee2c56d2f149f1bc1ce955d9"
-  end
 
   depends_on "rust" => :build
 
